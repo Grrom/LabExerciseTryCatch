4de6cc411266f48133b97620b0b059349e22a541
Jerome <josejeromelalunio@gmail.com>
2021-11-13 15:19:47 +0800

    add confirmation form

---
+++ Confirmation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LabExerciseTryCatch
+{
+    public partial class Confirmation : Form
+    {
+        public Confirmation()
+        {
+            InitializeComponent();
+        }
+    }
+}
